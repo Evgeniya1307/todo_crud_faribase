78a5bacefe48018ae0893f139e67c5954b487041
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-11-18 23:48:39 +0400

    если заголовок пуст он покажет новый заголовок

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,3 +1,24 @@
 //для задач
-import React from 'react';
+import React from "react";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 
+export default function Todo({
+    todo,
+    toggleComplete,
+    handleDelete,
+    handleEdit,
+}) {
+   const[newTitle, setTitle]=React.useState(todo.title);
+   
+   return(
+    <div className="todo">
+    <input
+    style={{textDecoration:todo.completed &&"line-through"}}
+    type="text"
+    value={todo.title === "" ? newTitle : todo.title}//если заголовок пуст он покажет новый заголовок 
+    />
+    </div>
+   )
+}
