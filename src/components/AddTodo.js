eb92cf3366db5ea63530012bcf7692c25f00772d
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2023-02-09 22:28:52 +0300

    updated editing function

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -4,19 +4,65 @@ import {collection, addDoc} from "firebase/firestore";
 
 
 //добавления документа
-export default  function AddTodo(){
-    const[title, setTitle] = React.useState("");
+export const  AddTodo = ({ task, tasks, setTasks, setTask }) => {
+  const [title, setTitle] = React.useState("");
+  const [message, setMessage] = React.useState("");
+  const [date, setDate] = React.useState("");
+  const [error, setError] = React.useState(false);
+
+    React.useEffect(() => {
+      if (Object.keys(task).length > 0) {
+        setTitle(task.title);
+        setMessage(task.message);
+        setDate(task.date);
+        console.log("A task  👨‍⚖️  ");
+      }
+    }, [task]);
+
+
+      const generateID = () => {
+        const id = Math.random().toString(20).substr(2);
+        return id;
+      };
+
 
     const handleSumbit=async(e)=>{
         e.preventDefault();
-        if (title !== ""){
+        if ([title, message, date ].includes("")){
             await addDoc(collection(db, "todos"),{ //фу-ия для добавления документа для хранения данных в базе данных
                title,
                completed:false, 
             });
             setTitle("");
-        }
+            setError(true)
+      return;
     };
+
+    setError(false);
+    const taskObject = {
+      title,
+      message,
+      date,
+      id: generateID(),
+    };
+
+    if (task.id) {
+        taskObject.id = task.id;
+        const updatedTasks = tasks.map((resp) =>
+          resp.id === task.id ? taskObject : resp
+        );
+        setTasks(updatedTasks);
+        setTask({});
+      } else {
+        taskObject.id = generateID();
+        setTasks([...tasks, taskObject]);
+      }
+      setTitle("");
+      setMessage("");
+      setDate("");
+    };
+
+
     return(
         <form onSubmit={handleSumbit}>
         <div className='input_container'>
@@ -33,3 +79,6 @@ export default  function AddTodo(){
         </form>
     );
 }
+
+
+export default AddTodo;
